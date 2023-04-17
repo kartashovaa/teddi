fd01c13ea912b86be60b3456987b9ba7ffaf0151
kartashov-a <kartashovandrey73@gmail.com>
2023-04-17 07:37:59 +0300

    Add task for collecting changed files

---
+++ src/main/kotlin/me/kyd3snik/test/diff/changes/FileChange.kt
@@ -0,0 +1,15 @@
+package me.kyd3snik.test.diff.changes
+
+import java.io.File
+import java.io.Serializable
+
+sealed class FileChange : Serializable {
+    /** related to current project */
+    abstract val file: File
+
+    data class Created(override val file: File) : FileChange()
+    data class Modified(override val file: File) : FileChange()
+
+    //TODO: do we really need deleted files?
+    data class Deleted(override val file: File) : FileChange()
+}
